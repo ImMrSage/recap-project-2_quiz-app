6fba4e60599cba5e1a3b0868e20808aaf1de6446
Sergii <96418850+ImMrSage@users.noreply.github.com>
2024-12-11 14:22:16 +0100

    make the answer button toggle between show and hide answer

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cardAnswer = document.querySelector('[data-js="card__answer"]');
 buttonBookmark.addEventListener("click", () => {
   buttonBookmark.classList.toggle("bookmark--active");
 });
+
 buttonAnswer.addEventListener("click", () => {
   cardAnswer.classList.toggle("card__answer--active");
+  if (cardAnswer.classList.contains("card__answer--active")) {
+    buttonAnswer.textContent = "Hide answer";
+  } else {
+    buttonAnswer.textContent = "Show answer";
+  }
 });
