02fe18094f53b0cced976c40aaf8d8abef50e168
Sergii <96418850+ImMrSage@users.noreply.github.com>
2024-12-11 14:07:39 +0100

    make show answer button show and hide answer on click[D

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // phew… not a lot going on here. Please add some code!
 const buttonBookmark = document.querySelector('[data-js="bookmark"]');
+const buttonAnswer = document.querySelector('[data-js="button-answer"]');
+const cardAnswer = document.querySelector('[data-js="card__answer"]');
 
 buttonBookmark.addEventListener("click", () => {
   buttonBookmark.classList.toggle("bookmark--active");
+});
+buttonAnswer.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--active");
 });
